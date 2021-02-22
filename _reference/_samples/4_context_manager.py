8e2d331ac0ab414e2547bebf5aa5b27b13ffe586
Timothy Hull <timothyhull@gmail.com>
2021-02-21 16:34:23 -0800

    Updated open() function arguments and indentation.

--- _reference/_samples/4_context_manager.py
+++ _reference/_samples/4_context_manager.py
@@ -2,13 +2,13 @@
 
 valid_file = False
 
-with open('my_file.txt', 'r') as file:
+with open('my_file.txt', mode='rt', encoding='utf-8') as file:
     contents = file.read()
-    if "Hello World".lower() in contents.lower():
+    if "Hello World" in contents.capitalize():
         valid_file = True
 
 if valid_file:
     print('File is valid\n'
-    f'Contents = {contents}')
+          f'Contents = {contents}')
 else:
     print('File is not valid')
